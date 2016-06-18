8e693ea567625b25104049c08a16a86669f1a8b7
Shi Ping <limshiping93@hotmail.com>
2016-06-18 22:32:13 +0800

    changed front page to recruitment typeform

---
+++ front-page-backup.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">    
+
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
+
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+
+  </head>
+
+  <body <?php body_class(); ?>>
+    
+
+
+
+      <!--================== CUSTOMIZED FRONTPAGE HEADER ==================-->
+      <header class="fpnavbar-head navbar navbar-default navbar-static-top" role="banner">
+        <div class="container">
+            <nav role="navigation">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="fpnavbar navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">ROOT</a>
+                </div>
+
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="fpnavbar collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="spacefillbesidebrand"></li>
+                    <?php wp_nav_menu( array('menu' => 'Primary Menu Left', 'container' => '','items_wrap' => '%3$s' )); ?>
+
+                  </ul>
+
+
+
+                  <ul class="fpnavbar nav navbar-nav navbar-right">
+                    <?php wp_nav_menu( array('menu' => 'Primary Menu Right', 'container' => '','items_wrap' => '%3$s' )); ?>
+                  </ul>  
+
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+    </header>
+    
+    <!--===================================================================-->
+
+    
+    <section class="centrallogo">
+      <div class="rootbigname">
+        <p class="rootverybig">ROOT</p>
+        <p class="sgmedium">SUTD Student Government</p>
+      </div>
+
+      <div class="centralbuttons">
+        <a class="btn btnleft" href="<?php echo get_permalink(110); ?>">Upcoming</a>
+        <a class="btn btnright" href="<?php echo get_permalink(81); ?>">Contact Us</a>
+      </div>  
+    </section>
+    
+
+    <footer class="frontpagefooter">
+      <p class="fpfootercontent">
+        <span class="organisationname">ROOT:SG</span><span>2016</span>
+      </p>
+    </footer>
+      
+
+    <div class="navbaranchortweak">
+      <div id="navbaranchor"></div>
+    </div>
+
+<?php wp_footer(); ?>
+
+<script type="text/javascript">
+<!--
+ var imlocation = "<?php bloginfo('stylesheet_directory'); ?>/images/homepage/";
+ var currentdate = 0;
+ var image_number = 0;
+ function ImageArray (n) {
+   this.length = n;
+   for (var i =1; i <= n; i++) {
+     this[i] = ' '
+   }
+ }
+ image = new ImageArray(3)
+ image[0] = 'background1.jpg'
+ // image[1] = 'background2.jpg'
+ image[1] = 'background3.jpg'
+ image[2] = 'background4.jpg'
+ var rand = 60/image.length
+ function randomimage() {
+  currentdate = new Date()
+  image_number = currentdate.getSeconds()
+  image_number = Math.floor(image_number/rand)
+  return(image[image_number])
+ }
+ document.write("<img class='bgM' src='" + imlocation + randomimage()+ "'>");
+//-->
+</script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-72760836-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</body>
+</html>
--- front-page.php
+++ front-page.php
@@ -1,124 +1,90 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">    
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
-		<title><?php bloginfo('name'); ?></title>
-		<?php wp_enqueue_script("jquery"); ?>
-		<?php wp_head(); ?>
+  <!--Add the title of your typeform below-->
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_enqueue_script("jquery"); ?>
+  <?php wp_head(); ?>
 
-		<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
-
-	</head>
-
-	<body <?php body_class(); ?>>
-			
-		<script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-72760836-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
-
-			<!--================== CUSTOMIZED FRONTPAGE HEADER ==================-->
-			<header class="fpnavbar-head navbar navbar-default navbar-static-top" role="banner">
-        <div class="container">
-            <nav role="navigation">
-              <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="fpnavbar navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">ROOT</a>
-                </div>
+  <!--CSS styles that ensure your typeform takes up all the available screen space (DO NOT EDIT!)-->
+  <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+  <style type="text/css">
+    html{
+      margin: 0;
+      height: 100%;
+      overflow: hidden;
+    }
+    iframe{
+      position: absolute;
+      left:0;
+      right:0;
+      bottom:0;
+      top:0;
+      border:0;
+    }
+  </style>
 
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="fpnavbar collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav">
-                    <li class="spacefillbesidebrand"></li>
-                    <?php wp_nav_menu( array('menu' => 'Primary Menu Left', 'container' => '','items_wrap' => '%3$s' )); ?>
+</head>
+<body <?php body_class(); ?>>
 
-                  </ul>
+<!--================== CUSTOMIZED FRONTPAGE HEADER ==================-->
+    <header class="fpnavbar-head navbar navbar-default navbar-static-top" role="banner">
+      <div class="container">
+          <nav role="navigation">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="fpnavbar navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">ROOT</a>
+              </div>
+
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="fpnavbar collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li class="spacefillbesidebrand"></li>
+                  <?php wp_nav_menu( array('menu' => 'Primary Menu Left', 'container' => '','items_wrap' => '%3$s' )); ?>
+
+                </ul>
 
 
 
-                  <ul class="fpnavbar nav navbar-nav navbar-right">
-                    <?php wp_nav_menu( array('menu' => 'Primary Menu Right', 'container' => '','items_wrap' => '%3$s' )); ?>
-                  </ul>  
+                <ul class="fpnavbar nav navbar-nav navbar-right">
+                  <?php wp_nav_menu( array('menu' => 'Primary Menu Right', 'container' => '','items_wrap' => '%3$s' )); ?>
+                </ul>  
 
-                </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
         </div>
-    </header>
-		
-		<!--===================================================================-->
-		<section class="centrallogo">
-			<div class="rootbigname">
-				<p class="rootverybig">ROOT</p>
-				<p class="sgmedium">SUTD Student Government</p>
-			</div>
+     </header>
+    
+    <!--===================================================================-->
+  <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://rootsutd.typeform.com/to/iWrIY0"></iframe>
+  <script type="text/javascript" src="https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js"></script>
+  <?php wp_footer(); ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			<div class="centralbuttons">
-				<a class="btn btnleft" href="<?php echo get_permalink(110); ?>">Upcoming</a>
-				<a class="btn btnright" href="<?php echo get_permalink(81); ?>">Contact Us</a>
-			</div>	
-		</section>
-		
+    ga('create', 'UA-72760836-1', 'auto');
+    ga('send', 'pageview');
 
-		<footer class="frontpagefooter">
-			<p class="fpfootercontent">
-				<span class="organisationname">ROOT:SG</span><span>2016</span>
-			</p>
-		</footer>
-			
-
-		<div class="navbaranchortweak">
-			<div id="navbaranchor"></div>
-		</div>
-
-<?php wp_footer(); ?>
-
-<script type="text/javascript">
-<!--
- var imlocation = "<?php bloginfo('stylesheet_directory'); ?>/images/homepage/";
- var currentdate = 0;
- var image_number = 0;
- function ImageArray (n) {
-   this.length = n;
-   for (var i =1; i <= n; i++) {
-     this[i] = ' '
-   }
- }
- image = new ImageArray(3)
- image[0] = 'background1.jpg'
- // image[1] = 'background2.jpg'
- image[1] = 'background3.jpg'
- image[2] = 'background4.jpg'
- var rand = 60/image.length
- function randomimage() {
- 	currentdate = new Date()
- 	image_number = currentdate.getSeconds()
- 	image_number = Math.floor(image_number/rand)
- 	return(image[image_number])
- }
- document.write("<img class='bgM' src='" + imlocation + randomimage()+ "'>");
-//-->
-</script>
-
+  </script>
 </body>
 </html>
