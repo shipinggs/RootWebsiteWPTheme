26ce41a7585df1505b0f016026c228466292b53d
Shi Ping <limshiping93@hotmail.com>
2016-09-27 11:27:53 +0800

    Functions: test POST api

--- functions.php
+++ functions.php
@@ -30,20 +30,37 @@ add_action('wp_enqueue_scripts', 'root_resources');
 
 // Theme setup
 function roottheme_setup() {
-
 	// Navigation Menus
 	register_nav_menus(array(
-
 		'primary' => __('Primary Menu Left'),
 		'primary right' => __('Primary Menu Right'),
 		'footer' => __('Footer Menu'),
 	));
-
-	
 }
 
 add_action('after_setup_theme', 'roottheme_setup');
 
+add_action( 'rest_api_init', function () {
+    // Register newsletter subscription API route
+    register_rest_route( 'sutdroot/v1', '/createpost', array(
+        'methods' => 'POST',
+        'callback' => 'api_create_post'
+    ) );
+} );
 
+function api_create_post() {
+  // Create post object
+  $my_post = array(
+    // 'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
+    'post_title'    => 'hello test',
+    'post_content'  => 'please publish <h1>Hello</h1>',
+    'post_status'   => 'publish',
+    'post_author'   => 1,
+    'post_category' => array( 13 )
+  );
+
+  // Insert the post into the database
+  return wp_insert_post( $my_post );
+}
 
 ?>
