99bc9a39bbac311a737f7e43c37b4bad8c6d7fcd
Shi Ping <limshiping93@hotmail.com>
2016-10-17 00:36:49 +0800

    About: updated to new exco mock

---
+++ page-about-backup.php
@@ -0,0 +1,295 @@
+<?php get_header(); ?>
+
+	<div class="about pagecontainer">
+		<div id="navbaranchor"></div>
+		<section class="abouthead">
+			<h1>WHAT WE DO</h1>
+			<p class="what-we-do">
+				We serve the student community. Our team is dedicated to enhancing every student's experience in school, in just about every aspect. We organize school-wide events, handle club and student feedback, implement solutions, and more.
+			</p>
+		</section>
+
+		<section class="ourteam">
+			<h1>OUR TEAM</h1>
+
+			<section class="exco committee">
+				<h2>Executive Committee</h2>
+				<div class="exco-separator"></div>
+				<div class="galleryofpeople">
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/chay.jpg" alt="President">
+						<div class="photodescription">
+							<h5>Chay Choong<br>President</h5>
+							<p>Class of 2017</p>
+							<!-- <a href="mailto:president@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/janet.jpg" alt="General Secretary">
+						<div class="photodescription">
+							<h5>Jisu Lee<br>General Secretary</h5>
+							<p>Class of 2017</p>
+							<!-- <a href="mailto:secretary@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/delin.jpg" alt="Finance Secretary">
+						<div class="photodescription">
+							<h5>Wang Delin<br>Finance Secretary</h5>
+							<p>Class of 2018</p>
+							<!-- <a href="mailto:finance@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/joei.jpg" alt="Vice President Events">
+						<div class="photodescription">
+							<h5>Joei Wee<br>Vice President<br>Events & Welfare</h5>
+							<p>Class of 2017</p>
+							<!-- <a href="mailto:vp_events@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/jai.jpg" alt="Vice President Comms">
+						<div class="photodescription">
+							<h5>Jai Atul Shah<br>Vice President<br>Comms & PR</h5>
+							<p>Class of 2017</p>
+							<!-- <a href="mailto:vp_comms@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/exco/jeza.jpg" alt="Vice President SI">
+						<div class="photodescription">
+							<h5>Jezamine Chua<br>Vice President<br>SI & Operations</h5>
+							<p>Class of 2016</p>
+							<!-- <a href="mailto:vp_ops@studentgov.sutd.edu.sg"><img class="excoemail" src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconblack.png"></a> -->
+						</div>
+					</div>
+				</div>
+			</section> <!-- exco -->
+
+			<section class="eventscomm committee">
+				<h2>Events & Welfare</h2>
+				<div class="committee-description">
+					<p>
+						Events and Welfare organises school-wide events for the benefit of the SUTD student population,
+						with an aim to establish Signature Events that students can look forward to every year.
+						More ad-hoc student welfare activities are organized by our sub-committee, the MINIONS.
+						We also render assistance to student-initiated events.<br>
+						<a href="mailto:events@studentgov.sutd.edu.sg"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconbluegrey.png"></a>
+					</p>
+					<div class="separator"></div>
+				</div>
+				<div class="galleryofpeople">
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/david.jpg" alt="Deputy VP">
+						<div class="photodescription">
+							<h5>David Yam<br>Strategic Planning Director</h5>
+							<p>Class of 2016</p>
+						</div>
+
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/felix.jpg" alt="Welfare Director">
+						<div class="photodescription">
+							<h5>Felix Soo<br>Welfare Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/yeeying.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Tan Yee Ying<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/gilbert.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Zhang Jiaxuan<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/zayar.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Zayar Lin<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/shalv.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Shalv Amit Parekh<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/zhanghao.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Zhang Hao<br>Project Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/clara.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Clara Calista<br>Project Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/zoe.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Ming Zhaozi<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/events/maylizabeth.jpg" alt="Project Director">
+						<div class="photodescription">
+							<h5>Maylizabeth Yap<br>Project Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+				</div>
+			</section> <!-- events -->
+
+			<section class="commscomm committee">
+				<h2>Communications & Public Relations</h2>
+				<div class="committee-description">
+					<p>
+						Communications & Public Relations aims to establish seamless communication channels within the school.
+						We listen to your feedback, work with the school to implement solutions, provide avenues of publicity,
+						and collaborate with businesses to bring you added benefits of being an SUTD student.<br>
+						<a href="mailto:communications@studentgov.sutd.edu.sg" style="text-decoration:none;"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconbluegrey.png"></a>
+					</p>
+					<div class="separator"></div>
+				</div>
+				<div class="galleryofpeople">
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/shiping.jpg" alt="Deputy VP">
+						<div class="photodescription">
+							<h5>Lim Shi Ping<br>Deputy Vice President</h5>
+							<p>Class of 2017</p>
+						</div>
+
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/dishi.jpg" alt="Public Relations Director">
+						<div class="photodescription">
+							<h5>Dishi Gautam<br>Public Relations Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/zouyun.jpg" alt="Public Relations Director">
+						<div class="photodescription">
+							<h5>Zou Yun<br>Public Relations Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/johsi.jpg" alt="Public Relations Director">
+						<div class="photodescription">
+							<h5>Keong Jo Hsi<br>Public Relations Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/lisa.jpg" alt="Publicity Director">
+						<div class="photodescription">
+							<h5>Lisa Koswara<br>Publicity Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/sidian.jpg" alt="Media Director">
+						<div class="photodescription">
+							<h5>Liu Sidian<br>Media Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/aditya.jpg" alt="Media Director">
+						<div class="photodescription">
+							<h5>Aditya Raju<br>Media Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/ryan.jpg" alt="Student Relations Director">
+						<div class="photodescription">
+							<h5>Ryan Tse<br>Student Relations Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/comms/liying.jpg" alt="Student Relations Director">
+						<div class="photodescription">
+							<h5>Zhu Liying<br>Student Relations Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+				</div>
+			</section> <!-- comms -->
+
+			<section class="sicomm committee">
+				<h2>Student Initiatives & Operations</h2>
+				<div class="committee-description">
+					<p>
+						Student Initiatives & Operations looks after and reviews the Fifth Row in SUTD,
+						creating frameworks and opportunities for clubs and interest groups to grow,
+						collaborate and maximise their potential within and beyond SUTD.<br>
+						<a href="mailto:si@studentgov.sutd.edu.sg" style="text-decoration:none;"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/emailiconbluegrey.png"></a>
+					</p>
+					<div class="separator"></div>
+				</div>
+				<div class="galleryofpeople">
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/si/gelin.jpg" alt="Deputy VP">
+						<div class="photodescription">
+							<h5>Liu Gelin<br>Deputy Vice President</h5>
+							<p>Class of 2017</p>
+						</div>
+
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/si/sikai.jpg" alt="Resource Management Director">
+						<div class="photodescription">
+							<h5>Chen Sikai<br>Resource Management Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/si/amos.jpg" alt="Student Initiative Director">
+						<div class="photodescription">
+							<h5>Amos Choo<br>Student Initiative Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/si/shruthi.jpg" alt="Student Initiative Director">
+						<div class="photodescription">
+							<h5>Shruthi Shangar<br>Student Initiative Director</h5>
+							<p>Class of 2018</p>
+						</div>
+					</div>
+					<div class="singleperson">
+						<img class="photo" src="<?php bloginfo('stylesheet_directory'); ?>/images/about/si/weisheng.jpg" alt="Student Initiative Director">
+						<div class="photodescription">
+							<h5>Chng Wei Sheng<br>Student Initiative Director</h5>
+							<p>Class of 2017</p>
+						</div>
+					</div>
+				</div>
+			</section> <!-- SI -->
+
+
+		</section><!-- our team -->
+
+
+	</div>
+
+
+<?php get_footer(); ?> 
